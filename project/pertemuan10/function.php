7821f8284fa4edf025f35c9e42dc3d4f742bb788
David Lim <davidskyeye@gmail.com>
2024-05-06 15:30:37 +0700

    menambahkan fitur iNSERT DATA

--- project/pertemuan10/function.php
+++ project/pertemuan10/function.php
@@ -22,3 +22,23 @@ function query($query)
 
   return $rows;
 }
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nik = htmlspecialchars($data['nik']);
+  $nama = htmlspecialchars($data['nama']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $alamat = htmlspecialchars($data['alamat']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nik', '$nama', '$jurusan', '$alamat', '$gambar');
+            ";
+  mysqli_query($conn, $query);
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
